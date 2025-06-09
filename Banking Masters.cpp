6225ae4613805ea2107f93b1beced016208ea370
MSK <tasneem.hany.2006@gmail.com>
2025-06-09 13:38:03 +0300

    Update NIXA Bank.cpp

--- Banking Masters.cpp
+++ Banking Masters.cpp
@@ -1,58 +1,102 @@
 #include <iostream>
 #include <string>
-#include <cstdlib> // «” Œœ«„ œ«·… system
-#include <fstream> //  ” Œœ„ ·· ⁄«„· „⁄ «·„·›« 
-#include <windows.h> // ·≈÷«›… «·√·Ê«‰
+#include <cstdlib>  
+#include <fstream>                         
+#include <windows.h>              
 
 using namespace std;
 
-// œ«·… · €ÌÌ— ·Ê‰ «·‰’
-void setColor(int color) {
+void setColor(int color)
+{
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 }
 
-void AccountMenu() {
-    setColor(11);
-    cout << "=======MENU=======" << endl;
-    setColor(14);
-    cout << "1: Create a new account " << endl;
-    cout << "2: Login " << endl;
-    cout << "3: Logout " << endl;
-    cout << "4: Withdraw money " << endl;
-    cout << "5: Deposit money " << endl;
-    cout << "6: Account details" << endl;
-    cout << "7: Exit " << endl;
-    setColor(11);
-    cout << "==================" << endl;
-    setColor(7);
+////////////////////////////////////////////////////////////////////////////////////
+
+void AccountMenu()
+{
+    system("cls");
+    setColor(11); 
+    cout << "\n";
+    cout << " ****************************************************\n";
+    cout << " *                                                  *\n";
+    cout << " *             WELCOME TO NEXA BANK SYSTEM          *\n";
+    cout << " *                                                  *\n";
+    cout << " ****************************************************\n";
+    cout << " *                                                  *\n";
+    cout << " *   1 - Create a new account                       *\n";
+    cout << " *   2 - Login to your account                      *\n";
+    cout << " *   3 - Logout                                     *\n";
+    cout << " *   4 - Withdraw money                             *\n";
+    cout << " *   5 - Deposit money                              *\n";
+    cout << " *   6 - View account details                       *\n";
+    cout << " *   7 - View transaction history                   *\n";
+    cout << " *   8 - Exit the system                            *\n";
+    cout << " *                                                  *\n";
+    cout << " ****************************************************\n";
+    setColor(7); // Ÿäÿ±ÿ¨ÿπ ŸÑŸÑŸàŸÜ ÿßŸÑÿßŸÅÿ™ÿ±ÿßÿ∂Ÿä
 }
 
-void NewAccount() {
+///////////////////////////////////////////////////////////////////////////////////////
+
+bool isIDExists(const string& ID)
+{
+    ifstream file("accounts.txt");
+    string fileID, fileUsername, filePassword;
+    double fileBalance;
+    while (file >> fileID >> fileUsername >> filePassword >> fileBalance)
+    {
+        if (fileID == ID)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+
+void NewAccount()
+{
     string name, password1, password2, ID;
-    setColor(10);
-    cout << "Please enter your name (without space): ";
+    setColor(10); // ÿ£ÿÆÿ∂ÿ± ŸÅÿßÿ™ÿ≠
+    cout << "Please enter your name: ";
     cin >> name;
     cout << "Enter your ID (12 digits): " << endl;
     cin >> ID;
-    while (true) {
-        if (ID.size() != 12) {
+    while (true)
+    {
+        if (ID.size() != 12)
+        {
+            setColor(12); // ÿ£ÿ≠ŸÖÿ±
+            cout << "ID must be exactly 12 digits. Try again: ";
+        }
+        else if (ID.find_first_not_of("0123456789") != string::npos)
+        {
             setColor(12);
-            cout << "ID must be exactly 12 digits and positive. Please try again: " << endl;
-        } else if (ID.find_first_not_of("0123456789") != std::string::npos) {
-            cout << "ID must contain only digits (0-9). Please try again: " << endl;
-        } else {
+            cout << "ID must contain only digits. Try again: ";
+        }
+        else if (isIDExists(ID))
+        {
+            setColor(12);
+            cout << "This ID already exists. Please enter a different one: ";
+        }
+        else
+        {
             break;
         }
         cin >> ID;
     }
+
     setColor(10);
     cout << "Please enter new password: ";
     cin >> password1;
     cout << "Please confirm password: ";
     cin >> password2;
-    while (password1 != password2) {
+    while (password1 != password2)
+    {
         setColor(12);
-        cout << "Passwords do not match, please try again." << endl;
+        cout << "Passwords don't match, please try again." << endl;
         setColor(10);
         cout << "Please enter new password: ";
         cin >> password1;
@@ -60,31 +104,41 @@ void NewAccount() {
         cin >> password2;
     }
 
-    ofstream file("accounts.txt", ios::app); // Õ›Ÿ «·»Ì«‰«  ›Ì „·›
-    file << ID << " " << name << " " << password1 << " 0" << endl;  // «·—’Ìœ Ì»œ√ » 0
-    file.close(); // ≈€·«ﬁ «·„·›
-    setColor(10);
-    cout << "Account created successfully!" << endl;
-    system("pause"); // ≈ŸÂ«— ¬Œ— —”«·…
-    system("cls"); //  ‰ŸÌ› «·‘«‘…
+    ofstream file("accounts.txt", ios::app);
+    file << ID << " " << name << " " << password1 << " 0" << endl;
+    file.close();
+    setColor(11);
+    cout << "Account created successfully " << endl;
+    setColor(7);
+    system("pause");
+    system("cls");
+    ofstream log("transactions_" + name + ".txt");
+    log << "Transaction history for " << name << ":\n";
+    log.close();
 }
 
-void login(string &currentUser, double &balance) {
+//////////////////////////////////////////////////////////////////////////////////////////
+
+void login(string& currentUser, double& balance)
+{
     string username, password, fileUsername, filePassword, fileID;
     double fileBalance;
-    setColor(14);
+    setColor(10);
     cout << "Enter username: ";
     cin >> username;
     cout << "Enter password: ";
     cin >> password;
-    ifstream file("accounts.txt"); // ﬁ—«¡… «·„·›
-    while (file >> fileID >> fileUsername >> filePassword >> fileBalance) {
-        if (fileUsername == username && filePassword == password) {
-            currentUser = username; // Ì÷„‰  ”ÃÌ· «·œŒÊ· „„« Ì”„Õ »⁄„·Ì«  «·”Õ» Ê«·≈Ìœ«⁄ œÊ‰ «·Õ«Ã… · ”ÃÌ· «·œŒÊ· „—… √Œ—Ï
+    ifstream file("accounts.txt");
+    while (file >> fileID >> fileUsername >> filePassword >> fileBalance)
+    {
+        if (fileUsername == username && filePassword == password)
+        {
+            currentUser = username;
             balance = fileBalance;
-            setColor(10);
-            cout << "Login successful!" << endl;
+            setColor(11);
+            cout << "Login successful" << endl;
             file.close();
+            setColor(7);
             system("pause");
             return;
         }
@@ -92,140 +146,244 @@ void login(string &currentUser, double &balance) {
     setColor(12);
     cout << "Invalid username or password!" << endl;
     file.close();
+    setColor(7);
     system("pause");
     system("cls");
 }
 
-void logout(string &currentUser, double &balance) {
+/////////////////////////////////////////////////////////////////////////////////////
+
+void logout(string& currentUser, double& balance)
+{
     currentUser = "";
     balance = 0;
-    setColor(10); // ·Ê‰ √Œ÷—
-    cout << "You have logged out successfully!" << endl;
+    setColor(10);
+    cout << "You have logged out successfully" << endl;
+    setColor(7);
     system("pause");
     system("cls");
 }
 
-void updateBalanceInFile(const string &currentUser, double newBalance) {
-    ifstream file("accounts.txt"); // ﬁ—«¡… «·»Ì«‰« 
-    ofstream tempFile("temp.txt"); // ‰› Õ „·› ÃœÌœ ·‰ﬂ » «·»Ì«‰«  ﬂ·Â« „⁄ «· ⁄œÌ·« 
+///////////////////////////////////////////////////////////////////////////////////
+
+void updateBalanceInFile(const string& currentUser, double newBalance)
+{
+    ifstream file("accounts.txt");
+    ofstream tempFile("temp.txt");
     string fileID, fileUsername, filePassword;
     double fileBalance;
-    while (file >> fileID >> fileUsername >> filePassword >> fileBalance) {
-        if (fileUsername == currentUser) {
-            fileBalance = newBalance;  //  ÕœÌÀ «·„»·€
+    while (file >> fileID >> fileUsername >> filePassword >> fileBalance)
+    {
+        if (fileUsername == currentUser)
+        {
+            fileBalance = newBalance;
         }
         tempFile << fileID << " " << fileUsername << " " << filePassword << " " << fileBalance << endl;
     }
     file.close();
     tempFile.close();
-    remove("accounts.txt"); // ‰Õ–› «·„·› «·ﬁœÌ„
-    rename("temp.txt", "accounts.txt"); // ‰⁄Ìœ  ”„Ì… «·„·› «·ÃœÌœ
+    remove("accounts.txt");
+    rename("temp.txt", "accounts.txt");
 }
 
-void Withdraw(string &currentUser, double &balance) {
+////////////////////////////////////////////////////////////////////////////////////////
+
+void Withdraw(string& currentUser, double& balance)
+{
     double amount;
-    setColor(14);
+    setColor(10);
     cout << "Enter the amount to withdraw: ";
     cin >> amount;
-    if (amount > 0 && amount <= balance) {
+    if (amount > 0 && amount <= balance)
+    {
         balance -= amount;
-        updateBalanceInFile(currentUser, balance); // ‰ÕœÀ «·„·› »⁄œ «· ⁄œÌ·
-        setColor(10);
+        updateBalanceInFile(currentUser, balance);
+        ofstream log("transactions_" + currentUser + ".txt", ios::app);
+        log << "Withdraw: -" << amount << ", Balance: " << balance << endl;
+        log.close();
+        setColor(11);
         cout << "Successfully withdrawn. Your new balance is: " << fixed << balance << endl;
-    } else {
+    }
+    else
+    {
         setColor(12);
         cout << "The amount is invalid or the money is not enough" << endl;
     }
+    setColor(7);
     system("pause");
     system("cls");
 }
 
-void deposit(string &currentUser, double &balance) {
+///////////////////////////////////////////////////////////////////////////////////////
+
+void deposit(string& currentUser, double& balance)
+{
     double amount;
-    setColor(14);
+    setColor(10);
     cout << "Enter the amount to deposit: ";
     cin >> amount;
-    if (amount > 0) {
+    if (amount > 0)
+    {
         balance += amount;
-        updateBalanceInFile(currentUser, balance); // ‰ÕœÀ «·„·› »⁄œ «· ⁄œÌ·
-        setColor(10);
+        updateBalanceInFile(currentUser, balance);
+        ofstream log("transactions_" + currentUser + ".txt", ios::app);
+        log << "Deposit: +" << amount << ", Balance: " << balance << endl;
+        log.close();
+        setColor(11);
         cout << "Successfully deposited. Your new balance is: " << fixed << balance << endl;
-    } else {
+    }
+    else
+    {
         setColor(12);
         cout << "The amount is invalid" << endl;
     }
+    setColor(7);
     system("pause");
     system("cls");
 }
 
-void viewAccountDetails(string currentUser, double balance) {
-    if (currentUser != "") {
+////////////////////////////////////////////////////////////////////////////////////
+
+void viewAccountDetails(string currentUser, double balance)
+{
+    if (currentUser != "")
+    {
         setColor(11);
         cout << "Account details for " << currentUser << ":" << endl;
-        setColor(14);
         cout << "Username: " << currentUser << endl;
         cout << "Balance: " << fixed << balance << endl;
-    } else {
+    }
+    else
+    {
         setColor(12);
         cout << "You need to log in first!" << endl;
     }
+    setColor(7);
     system("pause");
     system("cls");
 }
 
-int main() {
+////////////////////////////////////////////////////////////////////////////////////
+
+void viewTransactionHistory(const string& username)
+{
+    ifstream logfile("transactions_" + username + ".txt");
+    if (!logfile)
+    {
+        setColor(12);
+        cout << "No transaction history found." << endl;
+    }
+    else
+    {
+        string line;
+        setColor(11);
+        cout << "Transaction history for " << username << ":\n";
+        while (getline(logfile, line))
+        {
+            cout << line << endl;
+        }
+    }
+    logfile.close();
+    setColor(7);
+    system("pause");
+    system("cls");
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
     int choice;
     string currentUser = "";
     double balance = 0;
-    do {
+    char input;
+    do
+    {
         system("cls");
         AccountMenu();
-        setColor(14);
-        cout << "Enter your choice: ";
-        cin >> choice;
-        switch (choice) {
-            case 1:
-                NewAccount();
-                break;
-            case 2:
-                login(currentUser, balance);
-                break;
-            case 3:
-                logout(currentUser, balance);
-                break;
-            case 4:
-                if (currentUser != "") {
-                    Withdraw(currentUser, balance);
-                } else {
-                    setColor(12);
-                    cout << "You need to log in first" << endl;
-                    system("pause");
-                }
-                break;
-            case 5:
-                if (currentUser != "") {
-                    deposit(currentUser, balance);
-                } else {
-                    setColor(12);
-                    cout << "You need to log in first" << endl;
-                    system("pause");
-                }
-                break;
-            case 6:
-                viewAccountDetails(currentUser, balance);
-                break;
-            case 7:
-                setColor(10);
-                cout << "Exit " << endl;
-                cout << "Thank you for using our banking system!" << endl;
-                break;
-            default:
+        setColor(10);
+        cout << "Enter your choice (1-8): ";
+        cin >> input;
+        setColor(7);
+        if (input >= '1' && input <= '8')
+        {
+            choice = input - '0';
+        }
+        else
+        {
+            setColor(12);
+            cout << "Invalid input! Please enter a number between 1 and 8." << endl;
+            setColor(7);
+            system("pause");
+            continue;
+        }
+        switch (choice)
+        {
+        case 1:
+            NewAccount();
+            break;
+        case 2:
+            login(currentUser, balance);
+            break;
+        case 3:
+            logout(currentUser, balance);
+            break;
+        case 4:
+            if (currentUser != "")
+            {
+                Withdraw(currentUser, balance);
+            }
+            else
+            {
                 setColor(12);
-                cout << "Your choice is not available, try again." << endl;
+                cout << "You need to log in first" << endl;
+                setColor(7);
                 system("pause");
+            }
+            break;
+        case 5:
+            if (currentUser != "")
+            {
+                deposit(currentUser, balance);
+            }
+            else
+            {
+                setColor(12);
+                cout << "You need to log in first" << endl;
+                setColor(7);
+                system("pause");
+            }
+            break;
+        case 6:
+            viewAccountDetails(currentUser, balance);
+            break;
+        case 7:
+            if (currentUser != "")
+            {
+                viewTransactionHistory(currentUser);
+            }
+            else
+            {
+                setColor(12);
+                cout << "You need to log in first!" << endl;
+                setColor(7);
+                system("pause");
+            }
+            break;
+        case 8:
+            setColor(11);
+            cout << "Exit " << endl;
+            cout << "Thank you for using our banking system!" << endl;
+            setColor(7);
+            break;
+        default:
+            setColor(12);
+            cout << "Your choice is not available, try again." << endl;
+            setColor(7);
+            system("pause");
         }
         cout << endl;
-    } while (choice != 7);
+    } while (choice != 8);
 
     return 0;
 }
